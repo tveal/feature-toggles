d5f5438eb1c034415ef66c3a1e3decb88550ba0a
tveal <safariscout@gmail.com>
2019-03-02 10:55:01 -0600

    add sample toggles map for get-toggle endpoint

--- toggle-server/src/main/java/hello/ApiController.java
+++ toggle-server/src/main/java/hello/ApiController.java
@@ -3,9 +3,10 @@ package hello;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,13 @@ public class ApiController implements ErrorController {
 
     private static final String ERR_PATH = "/error";
 
+    private static final Map<String, Boolean> toggles = new HashMap<String, Boolean>() {
+        {
+            put("toggle1", true);
+            put("toggle2", false);
+        }
+    };
+
     /**
      * Sample url:
      * http://localhost:8090/toggle-server/get-toggle?toggleId=feature%2FPROJ-1234
@@ -22,12 +30,19 @@ public class ApiController implements ErrorController {
     @RequestMapping("/get-toggle")
     public String getToggle(@RequestParam("toggleId") String toggleId) {
         String returnValue;
+        
         try {
-            returnValue = URLDecoder.decode(toggleId, StandardCharsets.UTF_8.name());
+            String decodedToggleId = URLDecoder.decode(toggleId, StandardCharsets.UTF_8.name());
+            Boolean toggleValue = toggles.get(decodedToggleId);
+            if (toggleValue != null) {
+                returnValue = "Toggle value=" + toggleValue.booleanValue();
+            } else {
+                returnValue = "You requested value for toggleId: " + decodedToggleId;
+            }
         } catch(UnsupportedEncodingException e) {
             returnValue = "Invalid toggleId";
         }
-        return "You requested value for toggleId: " + returnValue;
+        return returnValue;
     }
 
     @RequestMapping(ERR_PATH)
